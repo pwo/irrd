42daa30daa5d41acbae7c1c6201a576ab16132cf
Sasha Romijn <github@mxsasha.eu>
2020-09-15 14:05:11 +0200

    ariadne tryout (requires master)

--- irrd/server/graphql/server.py
+++ irrd/server/graphql/server.py
@@ -1,27 +1,74 @@
-import os
+import time
 
-from aiohttp import web
-from tartiflette_aiohttp import register_graphql_handlers
+from ariadne import ObjectType, QueryType, gql, make_executable_schema
+from ariadne.asgi import GraphQL
 
-def run() -> None:
-    """
-    Entry point of the application.
-    """
-    web.run_app(
-        register_graphql_handlers(
-            app=web.Application(),
-            engine_sdl=os.path.dirname(os.path.abspath(__file__)) + "/",
-            engine_modules=[
-                "irrd.server.graphql.query_resolvers",
-            ],
-            executor_http_endpoint="/graphql",
-            executor_http_methods=["POST"],
-            graphiql_enabled=True,
-            subscription_ws_endpoint="/ws",
-        ),
-        host='127.0.0.1',
-        port=8000,
-    )
+from irrd.conf import config_init
+from irrd.server.query_resolver import QueryResolver
+from irrd.storage.database_handler import DatabaseHandler
+from irrd.storage.preload import Preloader
 
-if __name__ == '__main__':
-    run()
+config_init('/Users/sasha/dev/irrd4/local_config.yaml')
+dh = DatabaseHandler(readonly=True)
+pl = Preloader(enable_queries=True)
+qr = QueryResolver('', '', pl, dh)
+
+# Define types using Schema Definition Language (https://graphql.org/learn/schema/)
+# Wrapping string in gql function provides validation and better error traceback
+type_defs = gql("""
+    schema {
+      query: RPSLQuery
+    }
+    
+
+    
+    type RPSLQuery {
+      route(origin: Int): [RPSLRoute]
+    }
+    
+    type RPSLRoute {
+      prefix: String
+      origin: String
+      descr: String
+      remarks: String
+      mntBy: [String]
+      source: String
+      rpkiStatus: String
+    }
+""")
+
+# Map resolver functions to Query fields using QueryType
+rpsl_query = ObjectType("RPSLQuery")
+
+
+@rpsl_query.field("route")
+def resolve_query_route(_, info, origin):
+    for row in qr.rpsl_attribute_search('origin', f'AS{origin}'):
+        yield(dict(
+            prefix=row['parsed_data'][row['object_class']],
+            descr='\n'.join(row['parsed_data'].get('descr', [])),
+            origin=row['parsed_data']['origin'],
+            mntBy=row['parsed_data'].get('mnt-by', []),
+            remarks='\n'.join(row['parsed_data'].get('remarks', [])),
+            source=row['source'],
+            rpkiStatus=row['rpki_status'].name,
+        ))
+        yield(dict(
+            prefix=row['parsed_data'][row['object_class']],
+            descr='\n'.join(row['parsed_data'].get('descr', [])),
+            origin=row['parsed_data']['origin'],
+            mntBy=row['parsed_data'].get('mnt-by', []),
+            remarks='\n'.join(row['parsed_data'].get('remarks', [])),
+            source=row['source'],
+            rpkiStatus=row['rpki_status'].name,
+        ))
+
+
+# Map resolver functions to custom type fields using ObjectType
+rpsl_route = ObjectType("RPSLRoute")
+
+# Create executable GraphQL schema
+schema = make_executable_schema(type_defs, rpsl_query, rpsl_route)
+
+# Create an ASGI app using the schema, running in debug mode
+app = GraphQL(schema, debug=True)
